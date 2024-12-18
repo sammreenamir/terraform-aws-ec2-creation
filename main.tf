7db8964c2ac4c28e5388b618ae17277cf07e8fd1
Sammreen Amir <sammreen786@gmail.com>
2024-12-18 19:13:16 +0000

    new files added

---
+++ main.tf
@@ -0,0 +1,22 @@
+# Define AWS provider
+provider "aws" {
+  region = "us-east-1" # Change to your preferred AWS region
+}
+
+# Define AWS EC2 Instances
+resource "aws_instance" "example_vm" {
+  count         = 3 # Create 3 VMs
+  ami           = "ami-0e2c8caa4b6378d8c" # Replace with your valid AMI ID
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-vm-${count.index + 1}"
+  }
+}
+
+# Output the VM information
+output "instance_ips" {
+  description = "Public IP addresses of the VMs"
+  value       = aws_instance.example_vm[*].public_ip
+}
+
